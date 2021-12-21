b841500fadad4b6a234fdaef5766567907fd7ae8
Aude11 <vernet.aude@gmail.com>
2021-12-21 13:59:13 +0000

    add files to save items into db

---
+++ add_items_to_database.py
@@ -0,0 +1,25 @@
+import json
+import pymongo
+from datetime import datetime
+
+
+client = pymongo.MongoClient("mongodb+srv://admin:password@cluster0.ndyoh.mongodb.net/")
+data_base_name = "mycats"
+collection_name = "items"
+database = client[data_base_name]
+collection = database[collection_name]
+list_items_to_add_database = []
+file_to_read = open("data.txt", "r")
+
+for data_json in file_to_read:
+    data = json.loads(data_json)
+    data_date = data["date"]
+    date = datetime.strptime(data_date, "%a %b %d %H:%M:%S %Y")
+    data.update({"date": date})
+    list_items_to_add_database.append(data)
+    
+file_to_read.close()
+
+collection.insert_many(list_items_to_add_database)
+for item in collection.find():
+    print(item)
